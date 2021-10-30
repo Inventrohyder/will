fb7bb9a7c248664af66b447485f8a53b92ce6f12
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2021-10-30 21:05:10 +0200

    ✨ feat: show will history on the dashboard using cards

--- app/src/main/java/com/inventrohyder/will/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/inventrohyder/will/ui/dashboard/DashboardFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.inventrohyder.will.*
 import com.inventrohyder.will.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -17,6 +20,10 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val willViewModel: WillViewModel by viewModels {
+        WillViewModelFactory((activity?.application as WillApplication).repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,10 +36,20 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WillListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+
+        // Add an observer on the LiveData returned by getAlphabetizedWills.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        willViewModel.allWills.observe(viewLifecycleOwner) { wills ->
+            // Update the cached copy of the wills in the adapter.
+            wills.let { adapter.submitList(it) }
+        }
+
         return root
     }
 
