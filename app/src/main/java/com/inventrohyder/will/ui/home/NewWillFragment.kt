03451a880585dfb686e7a646b44579d7ee91aaeb
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2021-10-31 21:44:05 +0100

    ✨ feat: use an in-house rich editor

--- app/src/main/java/com/inventrohyder/will/ui/home/NewWillFragment.kt
+++ app/src/main/java/com/inventrohyder/will/ui/home/NewWillFragment.kt
@@ -1,22 +1,29 @@
 package com.inventrohyder.will.ui.home
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.TextWatcher
+import android.text.style.CharacterStyle
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.inventrohyder.will.*
 import com.inventrohyder.will.databinding.FragmentNewWillBinding
-import jp.wasabeef.richeditor.RichEditor
-import jp.wasabeef.richeditor.RichEditor.OnTextChangeListener
 
 class NewWillFragment : Fragment() {
 
-    private lateinit var editWordView: RichEditor
+    private lateinit var editWordView: EditText
     private var _binding: FragmentNewWillBinding? = null
 
     // This property is only valid between onCreateView and
@@ -36,7 +43,17 @@ class NewWillFragment : Fragment() {
         _binding = FragmentNewWillBinding.inflate(inflater, container, false)
         val root: View = binding.root
         editWordView = root.findViewById(R.id.edit_will)
-        editWordView.setOnTextChangeListener(OnTextChangeListener { text -> willText = text })
+        editWordView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                willText += s
+            }
+        })
 
         val button = root.findViewById<Button>(R.id.button_save)
         button.setOnClickListener {
@@ -56,15 +73,42 @@ class NewWillFragment : Fragment() {
         binding.toggleButton.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
             // Respond to button selection
             when (checkedId) {
-                R.id.action_bold -> editWordView.setBold()
-                R.id.action_italic -> editWordView.setItalic()
-                R.id.action_underline -> editWordView.setUnderline()
-                R.id.action_undo -> editWordView.undo()
-                R.id.action_redo -> editWordView.redo()
-                R.id.action_subscript -> editWordView.setSubscript()
-                R.id.action_superscript -> editWordView.setSuperscript()
+                R.id.action_bold -> setBold()
+                R.id.action_italic -> setItalic()
+                R.id.action_underline -> setUnderline()
+//                R.id.action_bold -> editWordView.setBold()
+//                R.id.action_italic -> editWordView.setItalic()
+//                R.id.action_underline -> editWordView.setUnderline()
+//                R.id.action_undo -> editWordView.undo()
+//                R.id.action_redo -> editWordView.redo()
+//                R.id.action_subscript -> editWordView.setSubscript()
+//                R.id.action_superscript -> editWordView.setSuperscript()
             }
         }
         return root
+    }
+
+
+    private fun setStyle(style: CharacterStyle) {
+        val spannableString: Spannable = SpannableStringBuilder(editWordView.text)
+        spannableString.setSpan(
+            style,
+            editWordView.selectionStart,
+            editWordView.selectionEnd,
+            0
+        )
+        editWordView.setText(spannableString)
+    }
+
+    private fun setBold() {
+        setStyle(StyleSpan(Typeface.BOLD))
+    }
+
+    private fun setItalic() {
+        setStyle(StyleSpan(Typeface.ITALIC))
+    }
+
+    private fun setUnderline() {
+        setStyle(UnderlineSpan())
     }
 }
