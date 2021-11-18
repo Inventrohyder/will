2d6acc39069d87f376cc7d65d7721c6785bd8625
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2021-11-18 06:01:26 +0100

    ✨ feat: use 3rd party editor with appropriate colors

--- app/src/main/java/com/inventrohyder/will/ui/home/NewWillFragment.kt
+++ app/src/main/java/com/inventrohyder/will/ui/home/NewWillFragment.kt
@@ -1,30 +1,23 @@
 package com.inventrohyder.will.ui.home
 
-import android.graphics.Typeface
-import android.os.Build
 import android.os.Bundle
-import android.text.Html
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.TextUtils
-import android.text.style.CharacterStyle
-import android.text.style.StyleSpan
-import android.text.style.UnderlineSpan
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.inventrohyder.will.*
 import com.inventrohyder.will.databinding.FragmentNewWillBinding
+import jp.wasabeef.richeditor.RichEditor
+
 
 class NewWillFragment : Fragment() {
 
-    private lateinit var editWordView: EditText
+    private lateinit var editWordView: RichEditor
     private var _binding: FragmentNewWillBinding? = null
 
     // This property is only valid between onCreateView and
@@ -44,32 +37,38 @@ class NewWillFragment : Fragment() {
         _binding = FragmentNewWillBinding.inflate(inflater, container, false)
         val root: View = binding.root
         editWordView = root.findViewById(R.id.edit_will)
-//        editWordView.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//                willText = s.toString()
-//            }
-//        })
+
+        // Match text editor background color to that chosen by the OS
+        val colorSurface = TypedValue()
+        requireContext().theme.resolveAttribute(R.attr.colorSurface, colorSurface, true)
+        editWordView.setEditorBackgroundColor(
+            colorSurface.data
+        )
+
+        // Match text editor font color to that chosen by the OS
+        val colorOnSurface = TypedValue()
+        requireContext().theme.resolveAttribute(R.attr.colorOnSurface, colorOnSurface, true)
+        editWordView.setEditorFontColor(
+            colorOnSurface.data
+        )
+
+        editWordView.setOnTextChangeListener { text ->
+            if (text.isEmpty()) {
+                editWordView.setPlaceholder(getString(R.string.hint_will))
+            }
+            willText = text
+        }
 
         val button = root.findViewById<Button>(R.id.button_save)
         button.setOnClickListener {
-            if (TextUtils.isEmpty(editWordView.text)) {
+            if (willText.isEmpty()) {
                 Toast.makeText(
                     context,
                     R.string.empty_not_saved,
                     Toast.LENGTH_LONG
                 ).show()
             } else {
-                val will = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    Will(Html.toHtml(editWordView.text, Html.TO_HTML_PARAGRAPH_LINES_INDIVIDUAL))
-                } else {
-                    Will(Html.toHtml(editWordView.text))
-                }
+                val will = Will(willText)
                 willViewModel.insert(will)
             }
             findNavController().navigate(R.id.navigation_home)
@@ -78,46 +77,16 @@ class NewWillFragment : Fragment() {
         binding.toggleButton.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
             // Respond to button selection
             when (checkedId) {
-                R.id.action_bold -> setBold()
-                R.id.action_italic -> setItalic()
-                R.id.action_underline -> setUnderline()
-//                R.id.action_bold -> editWordView.setBold()
-//                R.id.action_italic -> editWordView.setItalic()
-//                R.id.action_underline -> editWordView.setUnderline()
-//                R.id.action_undo -> editWordView.undo()
-//                R.id.action_redo -> editWordView.redo()
-//                R.id.action_subscript -> editWordView.setSubscript()
-//                R.id.action_superscript -> editWordView.setSuperscript()
+//                R.id.action_bold -> setBold()
+//                R.id.action_italic -> setItalic()
+//                R.id.action_underline -> setUnderline()
+                R.id.action_bold -> editWordView.setBold()
+                R.id.action_italic -> editWordView.setItalic()
+                R.id.action_underline -> editWordView.setUnderline()
+                R.id.action_undo -> editWordView.undo()
+                R.id.action_redo -> editWordView.redo()
             }
         }
         return root
-    }
-
-
-    private fun setStyle(style: CharacterStyle) {
-        val spannableString: Spannable = SpannableStringBuilder(editWordView.text)
-        spannableString.setSpan(
-            style,
-            editWordView.selectionStart,
-            editWordView.selectionEnd,
-            0
-        )
-        val cursorLocation = editWordView.selectionEnd
-        editWordView.setText(spannableString)
-
-        // Move the cursor to the old location
-        editWordView.setSelection(cursorLocation)
-    }
-
-    private fun setBold() {
-        setStyle(StyleSpan(Typeface.BOLD))
-    }
-
-    private fun setItalic() {
-        setStyle(StyleSpan(Typeface.ITALIC))
-    }
-
-    private fun setUnderline() {
-        setStyle(UnderlineSpan())
     }
 }
